6a6fe9d89a50d60b56e59b32a703bcb567ff62f7
pinky <pinky@gmail.com>
2021-09-04 18:33:05 +0530

    jobs

---
+++ src/test/java/jobs/AppTest.java
@@ -0,0 +1,20 @@
+package jobs;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
